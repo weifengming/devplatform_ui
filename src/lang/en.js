d46ac413599a01a68ab3ddd59ef3167516ddf7f0
weifengming <15611784834@163.com>
2020-03-02 21:36:21 +0800

    init

---
+++ src/lang/en.js
@@ -0,0 +1,15 @@
+export default {
+    route: {
+      dashboard: 'Dashboard',
+      introduction: 'Introduction'
+    },
+    role: {
+      permission: 'Permission'
+    },
+    user: {
+      uname: 'User Name',
+      role: 'Role',
+      sex: 'Sex'
+    }
+  }
+  
